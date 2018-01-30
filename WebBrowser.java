468b2132be803db638eddd75d04a86eebc09c692
jessicachen216 <35962146+jessicachen216@users.noreply.github.com>
2018-01-30 08:45:42 -0500

    Update WebBrowser.java

--- WebBrowser.java
+++ WebBrowser.java
@@ -1,1 +1,20 @@
-
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.geometry.Pos;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import java.util.ArrayList;
+public class WebBrowser extends Application
+{
+}
