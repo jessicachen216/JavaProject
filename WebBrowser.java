a53bfd1a674c849266d00352f92f0fba9eb5e2d6
STimothyDN <navarro18s@ncssm.edu>
2018-01-31 10:56:23 -0500

    Update WebBrowser.java

--- WebBrowser.java
+++ WebBrowser.java
@@ -15,6 +15,31 @@ import javafx.geometry.Pos;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import java.util.ArrayList;
-public class WebBrowser extends Application
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class WebBrowser extends Application 
 {
+  public static void main(String[] args) {
+	  launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+	  primaryStage.setTitle("java-buddy.blogspot.com");
+	
+	  WebView myBrowser = new WebView();
+	  WebEngine myWebEngine = myBrowser.getEngine();
+	  myWebEngine.load("http://java-buddy.blogspot.com/");
+	
+	  StackPane root = new StackPane();
+	  root.getChildren().add(myBrowser);
+	  primaryStage.setScene(new Scene(root, 640, 480));
+	  primaryStage.show();
+  }
 }
